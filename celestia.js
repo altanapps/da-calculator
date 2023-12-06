e9e031a50bbf137820ca8143ae977f1f80adae2c
altanapps <futureisaltan@gmail.com>
2023-12-06 12:56:41 +0100

    feat: fetch latest celestia gas price

--- celestia.js
+++ celestia.js
@@ -16,6 +16,15 @@ const FIRST_SPARSE_SHARE_CONTENT_SIZE =
 const CONTINUATION_SPARSE_SHARE_CONTENT_SIZE =
   SHARE_SIZE - NAMESPACE_SIZE - SHARE_INFO_BYTES;
 
+const CELESTIA_MAINNET = "https://explorer.modular.cloud/celestia-mainnet";
+const SELECTOR =
+  "#main-content > div > section:nth-child(2) > div > div > div.grid.grid-cols-2.tab:grid-cols-4.lg:grid-cols-5.[grid-template-areas:var(--grid-area-mobile)].tab:[grid-template-areas:var(--grid-area-tab)].lg:[grid-template-areas:var(--grid-area-lg)].auto-rows-[153px].tab:auto-rows-[146.5px].auto-cols-[145px].w-full.gap-8.tab:gap-10.font-medium.max-w-full.accent-primary > div.border-mid-dark-100.w-full.rounded-lg.border.shadow-sm.bg-white.p-4.flex.flex-col.items-start.gap-[1.125rem].md:gap-5.min-h-36.h-full.[grid-area:GP] > div.flex.flex-col.gap-2.items-start > p.text-base.tab:text-lg";
+
+// const XPATH = "/html/body/main/div/section[2]/div/div/div[2]/div[7]/div[2]";
+
+const axios = require("axios");
+const cheerio = require("cheerio");
+
 function sparseSharesNeeded(sequenceLen) {
   if (sequenceLen == 0) {
     return 0;
@@ -48,10 +57,31 @@ function defaultEstimateGas(blobSizes) {
   return estimateGas(blobSizes, 1, 0);
 }
 
-function estimateGas(blobSizes, gasPerByte, txSizeCost) {
+function estimateGasLimit(blobSizes, gasPerByte, txSizeCost) {
   return (
     gasToConsume(blobSizes, gasPerByte) +
     txSizeCost * BYTES_PER_BLOB_INFO * blobSizes.length +
     PFB_GAS_FIXED_COST
   );
+}
+
+function estimateGasFee() {
+  axios
+    .get("https://explorer.modular.cloud/celestia-mainnet")
+    .then((response) => {
+      const html = response.data;
+      console.log(html);
+
+      // Regular expression to capture the value after specific <p> tags
+      const regex =
+        /<p class="text-muted uppercase text-xxs tab:text-xs">GAS PRICE<\/p><p class="text-base tab:text-lg">([0-9.]+)<\/p>/;
+      const match = regex.exec(html);
+
+      if (match && match[1]) {
+        console.log(match[1]); // This should be your desired value
+        return match[1];
+      } else {
+        console.log("No match found");
+      }
+    });
 }
